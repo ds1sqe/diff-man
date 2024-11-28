a46861020df4646639ecd5f63ad035ab75cb1150
ds1sqe <ds1sqe@mensakorea.org>
2024-11-28 16:11:53 +0900

    add a line-start parser

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,8 @@ pub struct DiffComposition {
 pub struct Diff {
     pub path: PathBuf,
     pub hunk: Vec<DiffHunk>,
+    pub index: Option<String>, // TODO: type this
+    pub command: Option<String>,
 }
 pub struct DiffHunk {
     pub old_line: u64,
@@ -30,16 +32,133 @@ pub struct LineChange {
     kind: ChangeKind,
     content: String,
 }
+
+#[derive(Debug)]
 pub enum ChangeKind {
     Default,
     Added,
     Deleted,
 }
 
+#[derive(Debug)]
+pub enum LineStart {
+    Command,
+    Index,
+    OrignPath,
+    NewPath,
+    Hunk,
+    LineChange(ChangeKind),
+    Unknown,
+}
+
 struct Parser {}
+#[derive(Debug)]
+enum ParserState {
+    Init,
+    Command,
+    Index,
+    OriginPath,
+    NewPath,
+    Hunk,
+    LineChange,
+}
 
 impl Parser {
-    fn parse_git_udiff(src: &str) -> Diff {
+    fn parse_line_start(state: &ParserState, line: &str) -> LineStart {
+        match state {
+            ParserState::Init => {
+                if line.starts_with("diff") {
+                    LineStart::Command
+                } else {
+                    LineStart::Unknown
+                }
+            }
+            ParserState::Command => {
+                if line.starts_with("index") {
+                    LineStart::Index
+                } else if line.starts_with(DIFF_SIGN_HEADER_ORIGIN) {
+                    LineStart::OrignPath
+                } else {
+                    LineStart::Unknown
+                }
+            }
+            ParserState::Index => {
+                if line.starts_with(DIFF_SIGN_HEADER_ORIGIN) {
+                    LineStart::OrignPath
+                } else {
+                    LineStart::Unknown
+                }
+            }
+            ParserState::OriginPath => {
+                if line.starts_with(DIFF_SIGN_HEADER_NEW) {
+                    LineStart::NewPath
+                } else {
+                    LineStart::Unknown
+                }
+            }
+            ParserState::NewPath => {
+                if line.starts_with(DIFF_SIGN_HUNK) {
+                    LineStart::Hunk
+                } else {
+                    LineStart::Unknown
+                }
+            }
+            ParserState::Hunk => match line.split_at(1) {
+                (DIFF_SIGN_LINE_ADDED, _) => {
+                    LineStart::LineChange(ChangeKind::Added)
+                }
+                (DIFF_SIGN_LINE_DEFAULT, _) => {
+                    LineStart::LineChange(ChangeKind::Default)
+                }
+                (DIFF_SIGN_LINE_DELETED, _) => {
+                    LineStart::LineChange(ChangeKind::Deleted)
+                }
+                _ => LineStart::Unknown,
+            },
+            ParserState::LineChange => {
+                if line.starts_with("diff") {
+                    LineStart::Command
+                } else if line.starts_with("index") {
+                    LineStart::Index
+                } else if line.starts_with(DIFF_SIGN_HEADER_ORIGIN) {
+                    LineStart::OrignPath
+                } else if line.starts_with(DIFF_SIGN_HUNK) {
+                    LineStart::Hunk
+                } else {
+                    match line.split_at(1) {
+                        (DIFF_SIGN_LINE_ADDED, _) => {
+                            LineStart::LineChange(ChangeKind::Added)
+                        }
+                        (DIFF_SIGN_LINE_DEFAULT, _) => {
+                            LineStart::LineChange(ChangeKind::Default)
+                        }
+                        (DIFF_SIGN_LINE_DELETED, _) => {
+                            LineStart::LineChange(ChangeKind::Deleted)
+                        }
+                        _ => LineStart::Unknown,
+                    }
+                }
+            }
+        }
+    }
+
+    fn parse_git_udiff(src: &str) -> DiffComposition {
+        let mut state = ParserState::Init;
+        // State
+        //  command     diff --git a/tests/vm.rs b/tests/vm.rs
+        //  index       index 90d5af1..30044cb 100644
+        //  old_path    --- a/tests/vm.rs
+        //  new_path    +++ b/tests/vm.rs
+        //  hunk        @@ -16,7 +16,9 @@
+        //      linechange... |+|
+        //      linechange... |-|
+        //      linechange... | |
+        //      *hunk        @@ ...
+
+        for line in src.lines() {
+            let linestart = Self::parse_line_start(&state, line);
+            todo!()
+        }
         todo!()
     }
 }
@@ -51,9 +170,12 @@ impl DiffManager {
     }
 }
 
-#[test]
-fn parse_udiff() {
-    let tgt = r#"diff --git a/tests/vm.rs b/tests/vm.rs
+mod test {
+    use core::panic;
+
+    use crate::{Parser, ParserState};
+
+    const short_test_data: &str = r#"diff --git a/tests/vm.rs b/tests/vm.rs
 index 90d5af1..30044cb 100644
 --- a/tests/vm.rs
 +++ b/tests/vm.rs
@@ -88,4 +210,24 @@ index 90d5af1..30044cb 100644
          Some(Object::Int(Int { value: 30 })),
      ));
 "#;
+    #[test]
+    fn test_parse_linestart() {
+        let mut state = ParserState::Init;
+        for line in short_test_data.lines() {
+            let tag = Parser::parse_line_start(&state, line);
+            state = match &tag {
+                crate::LineStart::Command => ParserState::Command,
+                crate::LineStart::Index => ParserState::Index,
+                crate::LineStart::OrignPath => ParserState::OriginPath,
+                crate::LineStart::NewPath => ParserState::NewPath,
+                crate::LineStart::Hunk => ParserState::Hunk,
+                crate::LineStart::LineChange(change_kind) => {
+                    ParserState::LineChange
+                }
+                crate::LineStart::Unknown => panic!("Unknown line start"),
+            };
+
+            println!("[S:{:?}] [T:{:?}] -- L>{}", &state, &tag, &line);
+        }
+    }
 }
