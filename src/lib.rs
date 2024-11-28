cb27cb21d3160137691369f28f7ac6b57a70f59d
ds1sqe <ds1sqe@mensakorea.org>
2024-11-28 14:50:15 +0900

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+const DIFF_SIGN_LINE_ADDED: &str = "+";
+const DIFF_SIGN_LINE_DELETED: &str = "-";
+const DIFF_SIGN_LINE_DEFAULT: &str = " ";
+const DIFF_SIGN_HEADER_ORIGIN: &str = "---";
+const DIFF_SIGN_HEADER_NEW: &str = "+++";
+const DIFF_SIGN_HUNK: &str = "@@";
